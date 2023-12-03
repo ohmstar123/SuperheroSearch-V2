2653a674936cb2cc735334353e535ef6d24e43a8
ohmstar123 <opatel22@uwo.ca>
2023-12-03 16:08:44 -0500

    Created new page for admin (Currently doesn't have much in it)

---
+++ client/src/components/AdminPage/AdminPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AdminPage.css";
+
+const AdminPage = () => {
+    return(
+        <div>
+            <h1>Admin Page</h1>
+            <p>This is the admin page</p>
+        </div>
+    )
+}
+
+export default AdminPage;
